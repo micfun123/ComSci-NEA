d9eca14af8283e74b2048942037e2f943e7840b1
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-26 08:17:42 +0100

    v2

--- app.py
+++ app.py
@@ -1,53 +1,65 @@
 import pygame
-# initialize pygame
+import sys
+
 pygame.init()
 
-# define width of screen
-width = 1000
-# define height of screen
-height = 600
-screen_res = (width, height)
+# Screen dimensions
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_COLOR = (255, 255, 255)
 
-pygame.display.set_caption("GFG Bouncing game")
-screen = pygame.display.set_mode(screen_res)
+# Ball properties
+BALL_RADIUS = 20
+BALL_COLOR = (0, 0, 255)
+INITIAL_SPEED_X = 5
+DAMPING_FACTOR = 0.9
 
-# define colors
-red = (255, 0, 0)
-black = (0, 0, 0)
+# Initialize the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Ball Bouncing with Damping")
 
-# define ball
-ball_obj = pygame.draw.circle(
-	surface=screen, color=red, center=[100, 100], radius=5)
-# define speed of ball
-# speed = [X direction speed, Y direction speed]
-speed = [1, 1]
+# Initialize the ball
+ball_x, ball_y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
+ball_speed_x, ball_speed_y = INITIAL_SPEED_X, 0
 
-# game loop
+# Initialize the font
+font = pygame.font.Font(None, 30)
+
 while True:
-	# event loop
-	for event in pygame.event.get():
-		# check if a user wants to exit the game or not
-		if event.type == pygame.QUIT:
-			exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	# fill black color on screen
-	screen.fill(black)
+    # Update the ball's position
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
 
-	# move the ball
-	# Let center of the ball is (100,100) and the speed is (1,1)
-	ball_obj = ball_obj.move(speed)
-	# Now center of the ball is (101,101)
-	# In this way our wall will move
+    # Check for collisions with the screen edges
+    if ball_x + BALL_RADIUS >= SCREEN_WIDTH:
+        ball_x = SCREEN_WIDTH - BALL_RADIUS
+        ball_speed_x *= -DAMPING_FACTOR
 
-	# if ball goes out of screen then change direction of movement
-	if ball_obj.left <= 0 or ball_obj.right >= width:
-		speed[0] = -speed[0]
-	if ball_obj.top <= 0 or ball_obj.bottom >= height:
-		speed[1] = -speed[1]
+    if ball_x - BALL_RADIUS <= 0:
+        ball_x = BALL_RADIUS
+        ball_speed_x *= -DAMPING_FACTOR
 
-	# draw ball at new centers that are obtained after moving ball_obj
-	pygame.draw.circle(surface=screen, color=red,
-					center=ball_obj.center, radius=40)
+    if ball_y + BALL_RADIUS >= SCREEN_HEIGHT:
+        ball_y = SCREEN_HEIGHT - BALL_RADIUS
+        ball_speed_y *= -DAMPING_FACTOR
 
-	# update screen
-	pygame.display.flip()
+    if ball_y - BALL_RADIUS <= 0:
+        ball_y = BALL_RADIUS
+        ball_speed_y *= -DAMPING_FACTOR
+
+    # Draw the ball and the screen
+    screen.fill(SCREEN_COLOR)
+    pygame.draw.circle(screen, BALL_COLOR, (ball_x, ball_y), BALL_RADIUS)
+
+    # Display speed in the top left corner
+    speed_text = font.render("Speed: {:.2f}".format(abs(ball_speed_x)), True, (0, 0, 0))
+    screen.blit(speed_text, (10, 10))
+
+    pygame.display.flip()
+
+    # Control the frame rate
+    pygame.time.Clock().tick(60)
