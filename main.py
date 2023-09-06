f9fa5edb2e6ee5072b04470abf39c290e379d8e7
Michael Parker <michaelrbparker@protonmail.com>
2023-09-06 16:26:14 +0100

    vectore maths

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import math
 import random
 
 
-
 #screen
 screen_width = 800
 screen_height = 600
@@ -50,4 +49,50 @@ class vector2:
     def mag(self):
         return math.sqrt(self.x * self.x + self.y * self.y)
     
+    def dot(self, v):
+        return self.x * v.x + self.y * v.y
+    
+    def cross(self, v):
+        return self.x * v.y - self.y * v.x
 
+    def normalize(self):
+        mag = self.mag()
+        if mag != 0:
+            self.div(mag)
+
+    def limit(self, max_magnitude):
+        if self.mag() > max_magnitude:
+            self.normalize()
+            self.mult(max_magnitude)
+
+    @staticmethod
+    def distance(v1, v2):
+        dx = v1.x - v2.x
+        dy = v1.y - v2.y
+        return math.sqrt(dx * dx + dy * dy)
+    
+    def copy(self):
+        return vector2(self.x, self.y)
+
+    @classmethod
+    def clone(cls, v):
+        return cls(v.x, v.y)
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+#test vector2
+def test_vector2():
+    items = []
+    items.append(vector2(1, 2))
+    items.append(vector2(3, 4))
+    items.append(vector2(5, 6))
+    for i in items:
+        print(i)
+    print("distance: {}".format(vector2.distance(items[0], items[1])))
+    print("dot: {}".format(items[0].dot(items[1])))
+    print("cross: {}".format(items[0].cross(items[1])))
+    
+
+print("vector2 test")
+test_vector2()
